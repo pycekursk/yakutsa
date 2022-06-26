88e566ebe0b3a8f2f74cffce0353b50aa17483b6
Руслан <pycek@list.ru>
2022-06-26 12:51:45 +0300

    append mobile slide

--- wwwroot/js/images_slider.js
+++ wwwroot/js/images_slider.js
@@ -1,6 +1,95 @@
 ﻿(function ($) {
+  var touchStart = null; //Точка начала касания
+  var touchPosition = null; //Текущая позиция
+  const sensitivity = 20;
+
+  function TouchStart(e) {
+    //Получаем текущую позицию касания
+    touchStart = { x: e.changedTouches[0].clientX, y: e.changedTouches[0].clientY };
+    touchPosition = { x: touchStart.x, y: touchStart.y };
+  }
+
+  function TouchMove(e) {
+    //Получаем новую позицию
+    touchPosition = { x: e.changedTouches[0].clientX, y: e.changedTouches[0].clientY };
+  }
+
+  function TouchEnd(e, color) {
+    CheckAction(e); //Определяем, какой жест совершил пользователь
+    //Очищаем позиции
+    touchStart = null;
+    touchPosition = null;
+  }
+
+  function CheckAction(evt) {
+    var d = //Получаем расстояния от начальной до конечной точек по обеим осям
+    {
+      x: touchStart.x - touchPosition.x,
+      y: touchStart.y - touchPosition.y
+    };
+
+    var target = evt.path.find((e) => e.classList.contains("images-slider"));
+    if (!target) return;
+
+    let activeElement = target.querySelector("img.active");
+    let activeIndicator = target.querySelector(".indicator.active");
+
+    var msg = ""; //Сообщение
+
+    if (Math.abs(d.x) > Math.abs(d.y)) //Проверяем, движение по какой оси было длиннее
+    {
+      if (Math.abs(d.x) > sensitivity) //Проверяем, было ли движение достаточно длинным
+      {
+
+        let nextElement;
+        let nextActiveIndicator;
+        if (d.x > 0) //Если значение больше нуля, значит пользователь двигал пальцем справа налево
+        {
+          nextElement = activeElement.nextElementSibling;
+        }
+        else //Иначе он двигал им слева направо
+        {
+          nextElement = activeElement.previousElementSibling;
+        }
+
+        if (nextElement && nextElement instanceof HTMLImageElement) {
+          activeElement.classList.remove('active');
+          nextElement.classList.add('active');
+          activeElement = nextElement;
+
+          let url = nextElement.getAttribute('src');
+          nextActiveIndicator = target.querySelector(`.indicator[indicator-target="${url}"`);
+
+          activeIndicator.classList.remove('active');
+          nextActiveIndicator.classList.add('active');
+
+          activeIndicator = nextActiveIndicator;
+        }
+      }
+    }
+    else //Аналогичные проверки для вертикальной оси
+    {
+      if (Math.abs(d.y) > sensitivity) {
+        if (d.y > 0) //Свайп вверх
+        {
+          msg = "Swipe up";
+        }
+        else //Свайп вниз
+        {
+          msg = "Swipe down";
+        }
+      }
+    }
+  }
+
+
   function loadedHandler() {
-    if (document.body.classList.contains('mobile')) return;
+    //if (document.body.classList.contains('mobile')) return;
+
+    document.addEventListener("touchstart", function (e) { TouchStart(e); });
+    document.addEventListener("touchmove", function (e) { TouchMove(e); });
+    document.addEventListener("touchend", function (e) { TouchEnd(e, "green"); });
+    document.addEventListener("touchcancel", function (e) { TouchEnd(e, "red"); });
 
 
     var cards = document.querySelectorAll('.images-slider');
